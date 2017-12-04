1f52f953b6a43d3cf9083a7dfb0c79ca5596e56f
Tama McGlinn <t.mcglinn@gmail.com>
2017-12-04 10:04:54 +0100

    Created new .Net Core console application

---
+++ Zork/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
