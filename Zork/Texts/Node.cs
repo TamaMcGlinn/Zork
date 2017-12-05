d55d4bb87d62d554a525829b9a16530ebb1925e1
Tama McGlinn <t.mcglinn@gmail.com>
2017-12-05 14:28:10 +0100

    Added TextTree that loads a text file into a tree format

---
+++ Zork/Texts/Node.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zork.Texts
+{
+    class Node
+    {
+        private string _text;
+
+        public string Text
+        {
+            get { return _text; }
+            set { _text = value; }
+        }
+
+        private List<Node> _children;
+
+        public List<Node> Children
+        {
+            get { return _children; }
+        }
+
+        public Node(string text)
+        {
+            Text = text;
+            _children = new List<Node>();
+        }
+
+    }
+}
