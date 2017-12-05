a18bcc989728aac38cb8fd4bcbec5d28698f3519
= <=>
2017-12-05 09:50:12 +0100

    Renamed variable 'g' and instead of catching all exceptions catch specific exceptions in Program.Main

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -7,12 +7,24 @@ namespace Zork
         
         static void Main(string[] args)
         {
-            Game g = new Game();
+            Game gameObject = new Game();
             try
             {
-                g.Run();
-            } catch(Exception e)
+                gameObject.Run();
+            }
+            catch (StackOverflowException e)
             {
+                Console.WriteLine("Stackoverflow");
+                Console.WriteLine(e.Message);
+            }
+            catch (NullReferenceException e)
+            {
+                Console.WriteLine("Null reference");
+                Console.WriteLine(e.Message);
+            }
+            catch (OutOfMemoryException e)
+            {
+                Console.WriteLine("Out of memory");
                 Console.WriteLine(e.Message);
             }
         }
