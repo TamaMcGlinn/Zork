4fd620d3a374277d213bae434b53db11c28b78a8
Tama McGlinn <t.mcglinn@gmail.com>
2017-12-06 14:31:54 +0100

    Added test for healthvial

---
+++ ZorkUnitTest/PickupTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zork.Objects;
+
+namespace ZorkUnitTest
+{
+    [TestClass]
+    public class PickupTests
+    {
+        private HealthPickup makeVial()
+        {
+            return new HealthPickup("Vial", 30, "A bright green liquid in a thick glass vial.");
+        }
+
+        [TestMethod]
+        public void MakeHealthPickup()
+        {
+            var health = makeVial();
+            Assert.IsNotNull(health);
+        }
+
+        [TestMethod]
+        public void HealthIncreases()
+        {
+            var health = makeVial();
+            //Player p = new Player();
+        }
+    }
+}
