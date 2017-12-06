50372c9be1c5325c6e91d1140ba9d14107e33f80
Tama McGlinn <t.mcglinn@gmail.com>
2017-12-06 13:16:54 +0100

    Made maze class

--- Zork/Game.cs
+++ Zork/Game.cs
@@ -13,7 +13,7 @@ namespace Zork
     /// </summary>
     public class Game
     {
-        Room[,] maze;
+        Maze maze;
         Point currentRoom;
         List<Character> allCharacters;
         const int Width = 20;
@@ -24,156 +24,13 @@ namespace Zork
 
         public Game()
         {
-            createMaze();
+            currentRoom = new Point(StartX, StartY);
+            maze = new Maze(Width, Height, StartX, StartY);
+            maze.Print();
             allCharacters = new List<Character>();
             var barney = new Character("sherrif_barney", 3, 100, null, "A fat man in a prim black sherrif's uniform. He has a mustache and short brown hair.");
             allCharacters.Add(barney);
             maze[StartX, StartY].CharactersInRoom.Add(barney);
-        }
-
-        /// <summary>
-        /// Create a maze with at least one path between every two points.
-        /// </summary>
-        private void createMaze()
-        {
-            maze = new Room[Width, Height];
-            currentRoom = new Point(StartX, StartY);
-            maze[StartX, StartY] = new Room("Your house");
-            createNeighbour(currentRoom);
-            addExtraConnections(Width*Height);
-            printWholeMap();
-        }
-
-        private void addExtraConnections(int extras)
-        {
-            for(int i = 0; i < extras; ++i)
-            {
-                var roomToConnect = new Point(rng.Next(1, Width - 1), rng.Next(1, Height - 1));
-                int neighbourToConnect = rng.Next(0, 4);
-                var neighbourPoint = new Point(roomToConnect.X + (neighbourToConnect % 2)*((neighbourToConnect / 2) * 2 - 1), roomToConnect.Y + (1 - (neighbourToConnect % 2)) * ((neighbourToConnect / 2) * 2 - 1));
-                connect(roomToConnect, neighbourPoint);
-            }
-        }
-
-        /// <summary>
-        /// Set the canGoThere variable to true between the two locations.
-        /// </summary>
-        /// <param name="a"></param>
-        /// <param name="b"></param>
-        private void connect(Point a, Point b)
-        {
-            if(a.X == b.X)
-            {
-                if(a.Y+1 == b.Y)
-                {
-                    maze[a.X,a.Y].CanGoThere[Direction.South] = true;
-                    maze[b.X,b.Y].CanGoThere[Direction.North] = true;
-                } else
-                {
-                    Debug.Assert(a.Y - 1 == b.Y);
-                    maze[a.X, a.Y].CanGoThere[Direction.North] = true;
-                    maze[b.X, b.Y].CanGoThere[Direction.South] = true;
-                }
-            } else
-            {
-                if (a.X + 1 == b.X)
-                {
-                    maze[a.X, a.Y].CanGoThere[Direction.East] = true;
-                    maze[b.X, b.Y].CanGoThere[Direction.West] = true;
-                }
-                else
-                {
-                    Debug.Assert(a.X - 1 == b.X);
-                    maze[a.X, a.Y].CanGoThere[Direction.West] = true;
-                    maze[b.X, b.Y].CanGoThere[Direction.East] = true;
-                }
-            }
-        }
-
-        private void printWholeMap()
-        {
-            for (int yi = 0; yi < Height; ++yi)
-            {
-                for (int xi = 0; xi < Width; ++xi)
-                {
-                    Console.Write("0");
-                    if (maze[xi, yi].CanGoThere[Direction.East])
-                    {
-                        Debug.Assert(xi == Width-1 || maze[xi+1, yi].CanGoThere[Direction.West]);
-                        Console.Write("-");
-                    } else if(xi < Width - 1)
-                    {
-                        Console.Write(" ");
-                    }
-                }
-                Console.Write("\n");
-                if( yi < Height-1)
-                {
-                    for (int xi = 0; xi < Width; ++xi)
-                    {
-                        if (maze[xi, yi].CanGoThere[Direction.South])
-                        {
-                            Debug.Assert(yi == Height - 1 || maze[xi, yi+1].CanGoThere[Direction.North]);
-                            Console.Write("|");
-                        }
-                        else if (xi < Width - 1)
-                        {
-                            Console.Write(" ");
-                        }
-                        Console.Write(" ");
-                    }
-                    Console.Write("\n");
-                }
-            }
-        }
-
-        /// <summary>
-        /// Return the neighbouring points for which the room is null.
-        /// </summary>
-        /// <param name="place">The target location to examine</param>
-        /// <returns></returns>
-        private List<Point> listEmptyNeighbours(Point place)
-        {
-            List<Point> result = new List<Point>();
-            if(place.X > 0 && maze[place.X-1,place.Y] == null)
-            {
-                result.Add(new Point(place.X - 1, place.Y));
-            }
-            if (place.Y > 0 && maze[place.X, place.Y - 1] == null)
-            {
-                result.Add(new Point(place.X, place.Y - 1));
-            }
-            if(place.X < Width-1 && maze[place.X+1,place.Y] == null)
-            {
-                result.Add(new Point(place.X + 1, place.Y));
-            }
-            if (place.Y < Height-1 && maze[place.X, place.Y + 1] == null)
-            {
-                result.Add(new Point(place.X, place.Y + 1));
-            }
-            return result;
-        }
-
-        /// <summary>
-        /// Create rooms next to the current one as long as there are still null neighbours
-        /// </summary>
-        /// <param name="fromPoint"></param>
-        private void createNeighbour(Point fromPoint)
-        {
-            while(true)
-            {
-                List<Point> options = listEmptyNeighbours(fromPoint);
-                if (options.Count > 0)
-                {
-                    Point destPoint = options[rng.Next(0, options.Count)];
-                    maze[destPoint.X, destPoint.Y] = new Room("A busy street in London.");
-                    connect(fromPoint, destPoint);
-                    createNeighbour(destPoint); //recursive step
-                } else
-                {
-                    break;
-                }
-            }
         }
 
         /// <summary>
@@ -187,7 +44,7 @@ namespace Zork
             if( towards.X < 0 || towards.X == Width || towards.Y < 0 || towards.Y == Height)
             {
                 Console.WriteLine("You attempt to go " + direction.ToString().ToLower() + " but face the end of the world.");
-            } else if( maze[from.X,from.Y].CanGoThere[direction])
+            } else if( maze[from].CanGoThere[direction])
             {
                 currentRoom = towards;
             } else
@@ -203,7 +60,7 @@ namespace Zork
         {
             printInstructions();
             while (true) {
-                maze[currentRoom.X,currentRoom.Y].print();
+                maze[currentRoom].print();
                 string userInput = Console.ReadLine();
                 switch (userInput[0])
                 {
@@ -225,7 +82,7 @@ namespace Zork
                         break;
                     case 'L':
                     case 'l':
-                        Console.Write(maze[currentRoom.X, currentRoom.Y].LookAround());
+                        Console.Write(maze[currentRoom].LookAround());
                         break;
                     case 't':
                     case 'T':
@@ -233,7 +90,7 @@ namespace Zork
                         if (talkCommand.Length >= 3 && talkCommand[1] == "to")
                         {
                             string charactername = string.Join("_", talkCommand.Skip(2)).ToLower();
-                            Character talkTarget = maze[currentRoom.X, currentRoom.Y].CharactersInRoom.Find((Character c) => { return c.Name == charactername; });
+                            Character talkTarget = maze[currentRoom].CharactersInRoom.Find((Character c) => { return c.Name == charactername; });
                             if(talkTarget == null)
                             {
                                 Console.WriteLine("There is nobody called " + charactername + " here.");
