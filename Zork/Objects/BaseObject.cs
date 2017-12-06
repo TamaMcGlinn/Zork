f4a84986ac47358a67d2931c83058e5191a7c964
Tama McGlinn <t.mcglinn@gmail.com>
2017-12-06 14:03:05 +0100

    Added Clue and HealthPickup as subclasses of BaseObject

---
+++ Zork/Objects/BaseObject.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zork.Objects
+{
+    public abstract class BaseObject
+    {
+        #region properties
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        private string _description;
+
+        public string Description
+        {
+            get { return _description; }
+        }
+        #endregion properties
+
+        public BaseObject(string name, string description)
+        {
+            _name = name;
+            _description = description;
+        }
+    }
+}
---
+++ Zork/Objects/HealthPickup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zork.Objects
+{
+    public class HealthPickup : BaseObject
+    {
+        private int _potency;
+
+        public int Potency
+        {
+            get { return _potency; }
+        }
+
+        public HealthPickup(string name, int potency, string description) : base(name, description)
+        {
+            _potency = potency;
+        }
+    }
+}
