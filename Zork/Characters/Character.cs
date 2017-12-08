46d8b0b860f4ba5c494f4c0493fec9c470d84e8a
Tama McGlinn <t.mcglinn@gmail.com>
2017-12-08 13:43:23 +0100

    removed unused function

--- Zork/Characters/Character.cs
+++ Zork/Characters/Character.cs
@@ -147,27 +147,5 @@ namespace Zork
                 EquippedWeapon.PrintStats();
             }
         }
-
-        /// <summary>
-        /// Attempt to go from the from point to the towards point.
-        /// </summary>
-        /// <param name="from">From point</param>
-        /// <param name="towards">Destination point</param>
-        /// <param name="direction">Direction from from to towards</param>
-        public void TryGo(Maze maze, Point towards, Direction direction)
-        {
-            if (towards.X < 0 || towards.X == maze.Width || towards.Y < 0 || towards.Y == maze.Height)
-            {
-                Console.WriteLine("You attempt to go " + direction.ToString().ToLower() + " but face the end of the world.");
-            }
-            else if (maze[_location].CanGoThere[direction])
-            {
-                _location = towards;
-            }
-            else
-            {
-                Console.WriteLine("You cannot go " + direction.ToString().ToLower());
-            }
-        }
     }
 }
