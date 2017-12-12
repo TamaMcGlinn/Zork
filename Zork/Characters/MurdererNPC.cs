03d882dfe50b389e69889fe101f9f4cebf5f7c06
djsleurink <djsleurink@hotmail.com>
2017-12-12 16:21:03 +0100

    removed ismurderer boolean and added murdereNPC inherits from NNPC

---
+++ Zork/Characters/MurdererNPC.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zork.Objects;
+
+namespace Zork.Characters
+{
+    class MurdererNPC : NPC
+    {
+        public MurdererNPC(string name, string description, int strength, int startHealth, int letsPlayerFleePerXRounds, Weapon weapon = null, bool isMurderer = false) : base(name, description, strength, startHealth, letsPlayerFleePerXRounds, weapon)
+        {
+        }
+
+        public MurdererNPC(string name, string description, int strength, int startHealth, int maxHealth, int letsPlayerFleePerXRounds, Weapon weapon = null, bool isMurderer = false) : base(name, description, strength, startHealth, maxHealth, letsPlayerFleePerXRounds, weapon)
+        {
+        }
+    }
+}
