cf1a7305ba52af3831336bf9ea4c27ae9a92c675
Tama McGlinn <t.mcglinn@gmail.com>
2017-12-14 10:41:43 +0100

    Added code to test all available colours

---
+++ Zork/ColourDefinitions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Zork.UIContext;
+
+namespace Zork
+{
+    public static class ColourDefinitions
+    {
+        /// <summary>
+        /// Show all available console colours for background and foreground
+        /// </summary>
+        public static void TestColors()
+        {
+            foreach (ConsoleColor backgroundColor in Enum.GetValues(typeof(ConsoleColor)))
+            {
+                Console.BackgroundColor = backgroundColor;
+                foreach (ConsoleColor foregroundColor in Enum.GetValues(typeof(ConsoleColor)))
+                {
+                    Console.ForegroundColor = foregroundColor;
+                    ColourContext.WriteFullLine($"{foregroundColor} on {backgroundColor}");
+                }
+            }
+        }
+    }
+}
