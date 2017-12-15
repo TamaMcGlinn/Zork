82e0446019ae4e9b61df1bcfc37bf0ce9b89d9f9
Tama McGlinn <t.mcglinn@gmail.com>
2017-12-15 10:56:15 +0100

    added test for winning the game

--- ZorkUnitTest/InteractionTests.cs
+++ ZorkUnitTest/InteractionTests.cs
@@ -18,7 +18,7 @@ namespace ZorkUnitTest
             Point currentRoom = new Point(0, 0);
             Maze maze = new Maze(1, 1, currentRoom.X, currentRoom.Y);
             Player p = new Player(maze.Rooms[currentRoom.X,currentRoom.Y]);
-            NPC npc = createNPCBarney();
+            NPC npc = CreateNPCBarney();
             maze[currentRoom.X, currentRoom.Y].NPCsInRoom.Add(npc);
             StringWriter consoleOutput = new StringWriter();
             Console.SetOut(consoleOutput);
@@ -33,9 +33,23 @@ namespace ZorkUnitTest
             Maze maze = CreateMaze();
             Room room = maze.Rooms[0, 0];
             Player player = new Player(room);
-            NPC barney = createNPCBarney();
+            NPC barney = CreateNPCBarney();
             player.Fight(barney, game);
             Assert.IsTrue(player.Health <= 0);
+        }
+
+        [TestMethod]
+        public void WinGame()
+        {
+            StringWriter consoleOutput = new StringWriter();
+            Console.SetOut(consoleOutput);
+            Game game = new Game();
+            Maze maze = CreateMaze();
+            Room room = maze.Rooms[0, 0];
+            Player player = new Player(room);
+            NPC barney = CreateWeakMurderer();
+            player.Fight(barney, game);
+            Assert.IsTrue(consoleOutput.ToString().Contains("You win!"));
         }
 
         [TestMethod]
@@ -45,18 +59,26 @@ namespace ZorkUnitTest
             Maze maze = CreateMaze();
             Room room = maze.Rooms[0, 0];
             Player player = new Player(room);
-            NPC barney = CreateWeakNPC();
+            MurdererNPC barney = CreateWeakMurderer();
             player.Fight(barney, game);
             Assert.IsTrue(player.Health > 0);
         }
 
+        private MurdererNPC CreateWeakMurderer()
+        {
+            MurdererNPC npc = new MurdererNPC("constable_barney", "", 1, 10, 5, new Weapon("Strong weapon", 10, "desc"));
+            Room r = new Room("", new Point(0, 0));
+            npc.CurrentRoom = r;
+            r.NPCsInRoom.Add(npc);
+            return npc;
+        }
         public Maze CreateMaze()
         {
 
             return new Maze(10, 10, 0, 0);
         }
 
-        public NPC createNPCBarney()
+        public NPC CreateNPCBarney()
         {
             Room r = new Room("", new Point(0, 0));
             NPC barney = new NPC("constable_barney", "", 30, 100, 5, new Weapon("Strong weapon", 10, "desc"));
